af6fdb381fe4642531f9de95fa3dd77f1e0c3a00
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-16 08:17:52 -0400

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/entry.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
