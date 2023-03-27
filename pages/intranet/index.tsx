d16eb45f618bb3634d8b2db6e9c65cd8d4d4df6f
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-26 22:15:13 -0400

    add: intranet homepage start point

---
+++ pages/intranet/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>CU Intranet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+
+        <p className="mt-6 text-2xl">
+          Get started by editing{' '}
+          <code className="p-3 font-mono text-lg bg-blue-100 rounded-md">
+            pages/index.tsx
+          </code>
+        </p>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center gap-2"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
