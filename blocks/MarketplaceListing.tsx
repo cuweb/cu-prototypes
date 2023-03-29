cbf1e10df8f722fe6179973ea24f90ea246473a9
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-28 21:54:18 -0400

    add: blocks for intranet

---
+++ blocks/MarketplaceListing.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { MarketplaceItem, StackedList } from '@carletonuniversity/rds'
+import { MarketplaceData as marketplaceData } from '../data/MarketplaceData'
+
+export const MarketplaceListing = () => {
+  return (
+    <StackedList header="Marketplace" hasShadow>
+      {marketplaceData.map(
+        ({ id, title, link, image, alt, condition, cost, category }) => (
+          <MarketplaceItem key={id} as="li" link={link}>
+            <MarketplaceItem.Image image={image} alt={alt} />
+            <MarketplaceItem.Content>
+              <MarketplaceItem.Title title={title} link={link} />
+              <MarketplaceItem.Details condition={condition} cost={cost} />
+              <MarketplaceItem.Category category={category} />
+            </MarketplaceItem.Content>
+          </MarketplaceItem>
+        ),
+      )}
+    </StackedList>
+  )
+}
--- pages/intranet/index.tsx
+++ pages/intranet/index.tsx
@@ -1,62 +1,20 @@
 import type { NextPage } from 'next'
-// import Head from 'next/head'
-// import Image from 'next/image'
 
 import {
   Main,
   TopNav,
-  //   Banner,
   Section,
-  //   Container,
   FooterBasic,
   Column,
-  Aside,
   Panel,
-  StackedList,
-  EventItem,
   Container,
+  Heading,
 } from '@carletonuniversity/rds'
 
-import { EventData as eventData } from '../../data/EventData'
 import { EventsListing } from 'blocks/EventsListing'
-
-const SinglePara = () => {
-  return (
-    <>
-      <p>
-        <strong>Single Paragraph:</strong> Nobis voluptatem dolorum et eum
-        doloremque cupiditate velit. Praesentium architecto a distinctio aut
-        reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem
-        non. Molestiae quas dolores accusamus in. Praesent quis ligula quis
-        nulla malesuada tempor.
-      </p>
-    </>
-  )
-}
-
-const DoublePara = () => {
-  return (
-    <>
-      <p>
-        <strong>Double Paragraph:</strong> Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Aenean sit amet tortor pellentesque,
-        posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit
-        ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel
-        orci vulputate, eget vulputate neque porttitor. Suspendisse euismod,
-        urna et gravida volutpat, tortor risus vehicula nisl, in vulputate
-        lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus.
-        Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis,
-        quis pulvinar metus sapien in urna.
-      </p>
-      <p>
-        Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium
-        architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi
-        delectus nihil voluptatem non. Molestiae quas dolores accusamus in.
-        Praesent quis ligula quis nulla malesuada tempor.
-      </p>
-    </>
-  )
-}
+import { LinksListing } from 'blocks/LinksListing'
+import { AppsListing } from 'blocks/AppsListing'
+import { MarketplaceListing } from 'blocks/MarketplaceListing'
 
 const Home: NextPage = () => {
   return (
@@ -66,33 +24,23 @@ const Home: NextPage = () => {
       <Main>
         <Section>
           <Container bgColor="grey">
+            <Heading text="Dashboard" maxWidth="7xl" />
             <Column maxWidth="7xl" cols="1/3">
-              <Aside>
+              <div>
                 <Panel hasShadow>
                   <div className="p-4 h-96">UserInfo</div>
                 </Panel>
-                <Panel hasShadow>
-                  <div className="h-48 p-4">Panel</div>
-                </Panel>
-              </Aside>
+                <AppsListing />
+              </div>
+
               <div>
                 <EventsListing />
                 <Column cols="2">
                   <div>
-                    <Panel hasShadow>
-                      <div className="h-32 p-4">Panel</div>
-                    </Panel>
-                    <Panel hasShadow>
-                      <div className="p-4 h-96">Panel</div>
-                    </Panel>
+                    <MarketplaceListing />
                   </div>
                   <div>
-                    <Panel hasShadow>
-                      <div className="h-48 p-4">Panel</div>
-                    </Panel>
-                    <Panel hasShadow>
-                      <div className="p-4 h-60">Panel</div>
-                    </Panel>
+                    <LinksListing />
                   </div>
                 </Column>
               </div>
