208061a433ca5e0114b732027709c099c3c9572d
Troy Chaplin <troy.chaplin@gmail.com>
2023-04-17 10:18:07 -0400

    change: created new component folder

---
+++ components/ListingMarketMeta/ListingMarketMeta.tsx
@@ -0,0 +1,29 @@
+import { metaStyles } from './Listing.Styles'
+import { BanknotesIcon } from '@heroicons/react/24/outline'
+
+export interface ListingMarketMetaProps {
+  condition?: string
+  cost?: string
+}
+
+export const ListingMarketMeta = ({
+  condition = 'not specified',
+  cost = 'Free',
+}: ListingMarketMetaProps) => {
+  return (
+    <>
+      {cost && (
+        <p className={`${metaStyles.itemLarge} ${metaStyles.alignIcon}`}>
+          <BanknotesIcon className={metaStyles.iconLarge} aria-hidden="true" />$
+          {cost}
+        </p>
+      )}
+
+      <ul className={metaStyles.wrapper}>
+        <li className={metaStyles.item}>
+          {condition} condition | Price is flexible
+        </li>
+      </ul>
+    </>
+  )
+}
