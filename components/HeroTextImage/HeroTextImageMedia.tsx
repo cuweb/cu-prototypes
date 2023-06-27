e9ae0c44d39975d92bd9a6f4d2be6f0f8993a945
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-27 06:24:14 -0400

    change: minor updates

--- components/HeroTextImage/HeroTextImageMedia.tsx
+++ components/HeroTextImage/HeroTextImageMedia.tsx
@@ -4,14 +4,23 @@ import { styles } from './HeroTextImage.Styles'
 export interface HeroTextImageMediaProps {
   children?: React.ReactNode
   image?: string
+  imageWidth?: '25' | '33' | '40' | '50'
   angle?: 'left' | 'right' | 'full'
   focalPointX?: string
   focalPointY?: string
 }
 
+export const rdsImageWidth = {
+  25: 'md:w-3/12',
+  33: 'md:w-4/12',
+  40: 'md:w-5/12',
+  50: 'md:w-6/12',
+}
+
 export const HeroTextImageMedia = ({
   children,
   image,
+  imageWidth = '40',
   angle,
   focalPointX = '50',
   focalPointY = '50',
@@ -24,7 +33,10 @@ export const HeroTextImageMedia = ({
   }
 
   return (
-    <div className={`${styles.mediaWrapper} ${hasImage}`} style={inlineStyle}>
+    <div
+      className={`${styles.mediaWrapper} ${rdsImageWidth[imageWidth]} ${hasImage}`}
+      style={inlineStyle}
+    >
       {angle === 'full' && (
         <svg
           xmlns="http://www.w3.org/2000/svg"
