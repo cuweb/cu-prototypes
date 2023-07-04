013480c73c1583b12d25a2c1e340ad2792cbf816
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2023-07-04 14:06:05 -0400

    refactor(plusnav): support dropdown menus

--- components/PlusBar/PlusBarSideNav.tsx
+++ components/PlusBar/PlusBarSideNav.tsx
@@ -51,18 +51,16 @@ export const PlusBarSideNav = ({ sideMenu }: any) => {
                           />
                         </Disclosure.Button>
                         <Disclosure.Panel className="mt-2 space-y-2">
-                          {sideMenuItem.subMenu.map(
-                            (navSubMenuItem: any) => (
-                              <Disclosure.Button
-                                key={navSubMenuItem.id}
-                                as="a"
-                                href={navSubMenuItem.link}
-                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                              >
-                                {navSubMenuItem.label}
-                              </Disclosure.Button>
-                            ),
-                          )}
+                          {sideMenuItem.subMenu.map((navSubMenuItem: any) => (
+                            <Disclosure.Button
+                              key={navSubMenuItem.id}
+                              as="a"
+                              href={navSubMenuItem.link}
+                              className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >
+                              {navSubMenuItem.label}
+                            </Disclosure.Button>
+                          ))}
                         </Disclosure.Panel>
                       </>
                     )}
