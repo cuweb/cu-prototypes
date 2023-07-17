d76cdbbdb59e62c206e693bb68567d74d0500272
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-07-17 14:02:04 -0400

    update: change listing component from rds to project source

--- pages/cutheme/search.tsx
+++ pages/cutheme/search.tsx
@@ -7,13 +7,14 @@ import {
   Heading,
   Card,
   HeroTextImage,
-  Listing,
   SearchForm,
   Pagination,
   StackedList,
   Aside,
   Container,
 } from '@carletonuniversity/rds'
+
+import { Listing } from '@components/Listing/Listing'
 
 import React, { useState, useCallback } from 'react'
 
@@ -57,50 +58,45 @@ const Home: NextPage = () => {
 
       <Main>
         <Section hasProse>
+          <HeroTextImage maxWidth="5xl">
+            <HeroTextImage.Content title="Site Search" />
+          </HeroTextImage>
           <Container>
-            <HeroTextImage maxWidth="5xl">
-              <HeroTextImage.Content title="Site Search" />
-            </HeroTextImage>
             <SearchBar />
-            <Column cols="2/3">
-              <Column.Content>
-                <StackedList>
-                  {NewsData.slice(0, 5).map(
-                    ({ id, title, link, date, image, alt, excerpt }) => (
-                      <Listing key={id}>
-                        <a href="https://carleton.ca/webservices">
-                          <Listing.Content>
-                            <Listing.PostMeta date={date} />
-                            <Listing.Header text={title} />
-                            <Listing.Excerpt text={excerpt} />
-                          </Listing.Content>
-                        </a>
-                      </Listing>
-                    ),
-                  )}
-                </StackedList>
-              </Column.Content>
-              <Column.Content>
-                <Aside>
-                  <StackedList hasBorder>
-                    <li className="text-sm px-6 py-5">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Aliquam augue turpis, feugiat vitae viverra in, egestas
-                      vitae nulla.
-                    </li>
-                    <li className="text-sm px-6 py-5">
-                      Quisque auctor ultrices mauris, et semper urna aliquam
-                      quis.
-                    </li>
-                    <li className="text-sm px-6 py-5">
-                      Suspendisse vestibulum hendrerit.
-                    </li>
-                  </StackedList>
-                </Aside>
-              </Column.Content>
-            </Column>
-            <PageSelect />
           </Container>
+          <Column cols="2/3">
+            <StackedList>
+              {NewsData.slice(0, 5).map(
+                ({ id, title, link, date, image, alt, excerpt, tags }) => (
+                  <Listing key={id}>
+                    <a href="https://carleton.ca/webservices">
+                      <Listing.Content>
+                        <Listing.PostMeta date={date} />
+                        <Listing.Header text={title} />
+                        <Listing.Excerpt text={excerpt} />
+                      </Listing.Content>
+                    </a>
+                  </Listing>
+                ),
+              )}
+            </StackedList>
+            <Aside>
+              <StackedList hasBorder>
+                <li className="text-sm px-6 py-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam augue turpis, feugiat vitae viverra in, egestas vitae
+                  nulla.
+                </li>
+                <li className="text-sm px-6 py-5">
+                  Quisque auctor ultrices mauris, et semper urna aliquam quis.
+                </li>
+                <li className="text-sm px-6 py-5">
+                  Suspendisse vestibulum hendrerit.
+                </li>
+              </StackedList>
+            </Aside>
+          </Column>
+          <PageSelect />
         </Section>
       </Main>
 
