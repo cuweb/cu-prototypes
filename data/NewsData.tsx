e83ac8a9dda93b3b01ca280e685af85c5e79d72c
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-07-20 11:56:33 -0400

    update: comment out images from data to hide featured images from search listings

--- data/NewsData.tsx
+++ data/NewsData.tsx
@@ -3,7 +3,7 @@ export const NewsData = [
     id: 1,
     title: ' How to Write for the Web',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
@@ -41,7 +41,7 @@ export const NewsData = [
     id: 3,
     title: '101 Ways to Cook Ramen',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Attend this special back-to-school event on how to cook ramen in your dorm room. Hosted by world renowned chef Gorden Ramsey you will learn the best there is in fine noodle cuisine.',
@@ -60,7 +60,7 @@ export const NewsData = [
     id: 4,
     title: ' How to Write for the Web',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
@@ -79,7 +79,7 @@ export const NewsData = [
     id: 5,
     title: 'Captivating Captions: Why We Use Captions on Videos',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu. Suspendisse potenti. Nulla varius, enim nec maximus.',
@@ -98,7 +98,7 @@ export const NewsData = [
     id: 6,
     title: 'End-of-Year Reflections and Resolutions for Our Web Services Team',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nulla eget est viverra, et tristique velit elementum. Aliquam ultrices vitae felis sit amet pharetra. Vivamus vitae dui mi. Quisque vitae nibh at orci lobortis rhoncus eu at mi. Nulla facilisi. In hac habitasse platea dictumst. Curabitur sed lectus risus.',
@@ -122,7 +122,7 @@ export const NewsData = [
     id: 7,
     title: 'End-of-Year Reflections and Resolutions for Our Web Services Team',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nulla eget est viverra, et tristique velit elementum. Aliquam ultrices vitae felis sit amet pharetra. Vivamus vitae dui mi. Quisque vitae nibh at orci lobortis rhoncus eu at mi. Nulla facilisi. In hac habitasse platea dictumst. Curabitur sed lectus risus.',
@@ -146,7 +146,7 @@ export const NewsData = [
     id: 8,
     title: ' How to Write for the Web',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
@@ -165,7 +165,7 @@ export const NewsData = [
     id: 9,
     title: 'Captivating Captions: Why We Use Captions on Videos',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu. Suspendisse potenti. Nulla varius, enim nec maximus.',
@@ -184,7 +184,7 @@ export const NewsData = [
     id: 10,
     title: ' How to Write for the Web',
     link: '#',
-    image: '/images/news-img.jpg',
+    // image: '/images/news-img.jpg',
     alt: 'Image alt text',
     excerpt:
       'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
