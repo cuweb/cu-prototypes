c271aeb55a74f3bcf62a7748e5e5a167b30c6bf7
Troy Chaplin <troy.chaplin@gmail.com>
2023-07-20 15:42:49 -0300

    add: search data

---
+++ data/SearchData.tsx
@@ -0,0 +1,315 @@
+export const SearchData = [
+  {
+    id: 1,
+    type: 'news',
+    title: 'End-of-Year Reflections and Resolutions for Our Web Services Team',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nulla eget est viverra, et tristique velit elementum. Aliquam ultrices vitae felis sit amet pharetra. Vivamus vitae dui mi. Quisque vitae nibh at orci lobortis rhoncus eu at mi. Nulla facilisi. In hac habitasse platea dictumst. Curabitur sed lectus risus.',
+    date: '2022-12-23 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+  {
+    id: 2,
+    type: 'news',
+    title: ' How to Write for the Web',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
+    date: '2022-12-14 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category Three',
+          slug: 'category-three',
+        },
+      ],
+    },
+  },
+  {
+    id: 3,
+    type: 'event',
+    title: 'Ottawa Trivia Session #2',
+    link: '#',
+    startDate: '2023-03-31 20:00:00',
+    endDate: '2023-03-31 11:00:00',
+    event_address: '71953 Schmidt Row Apt. 896\nLaurianefurt, TN 88829-0223',
+    on_campus: true,
+    on_campus_building: 'Robertson Hall',
+    on_campus_room_number: '420',
+    image: 'https://source.unsplash.com/random/400x175',
+    alt: `Images need alt text`,
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+      audience: [
+        {
+          id: 1,
+          name: 'Student',
+          slug: 'student',
+        },
+        {
+          id: 2,
+          name: 'Faculty',
+          slug: 'faculty',
+        },
+      ],
+    },
+    multiday: false,
+  },
+  {
+    id: 4,
+    type: 'people',
+    firstName: 'Carlton',
+    lastName: 'Banks',
+    link: '#',
+    image: '/images/people-img.jpg',
+    alt: 'Image needs alt text',
+    jobTitle: 'Manager, Peacock Stop',
+    email: 'carlton.banks@freshprince.tv',
+    phone: '613-520-2600',
+    phoneExt: '3825',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+  {
+    id: 5,
+    type: 'people',
+    firstName: 'Philip',
+    lastName: 'Banks',
+    link: '#',
+    image: '/images/people-img.jpg',
+    alt: 'Image needs alt text',
+    jobTitle: 'Judge, Lawyer, Activist',
+    email: 'philip.banks@freshprince.tv',
+    phone: '613-520-2600',
+    phoneExt: '3825',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+  {
+    id: 6,
+    type: 'news',
+    title: 'Captivating Captions: Why We Use Captions on Videos',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu. Suspendisse potenti. Nulla varius, enim nec maximus.',
+    date: '2022-11-25 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 4,
+          name: 'Category Four',
+          slug: 'category-four',
+        },
+      ],
+    },
+  },
+  {
+    id: 7,
+    type: 'news',
+    title: ' How to Write for the Web',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Proin placerat non nisl sed ullamcorper. Maecenas ultrices diam a lorem feugiat feugiat. Curabitur ultrices egestas tortor vel feugiat. Mauris vitae massa vulputate, dignissim ipsum interdum, maximus nunc.',
+    date: '2022-12-14 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category Three',
+          slug: 'category-three',
+        },
+      ],
+    },
+  },
+  {
+    id: 8,
+    type: 'event',
+    title: 'Carleton University Escape Room',
+    link: '#',
+    startDate: '2023-04-02 19:30:00',
+    endDate: '2023-04-07 20:00:00',
+    event_address: '71953 Schmidt Row Apt. 896\nLaurianefurt, TN 88829-0223',
+    on_campus: false,
+    on_campus_building: null,
+    on_campus_room_number: null,
+    image: 'https://source.unsplash.com/random/400x175',
+    alt: `Images need alt text`,
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+      audience: [
+        {
+          id: 1,
+          name: 'Student',
+          slug: 'student',
+        },
+        {
+          id: 2,
+          name: 'Faculty',
+          slug: 'faculty',
+        },
+      ],
+    },
+    multiday: true,
+  },
+  {
+    id: 9,
+    type: 'news',
+    title: 'Captivating Captions: Why We Use Captions on Videos',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu. Suspendisse potenti. Nulla varius, enim nec maximus.',
+    date: '2022-11-25 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 4,
+          name: 'Category Four',
+          slug: 'category-four',
+        },
+      ],
+    },
+  },
+  {
+    id: 10,
+    type: 'video',
+    title: 'Carleton Battle of the Bands - A Hit!',
+    link: '#',
+    alt: 'Image alt text',
+    excerpt:
+      'Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu. Suspendisse potenti. Nulla varius, enim nec maximus.',
+    date: '2022-12-23 10:27:31',
+    video: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+    tags: {
+      category: [
+        {
+          id: 4,
+          name: 'Video',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+  {
+    id: 11,
+    type: 'people',
+    firstName: 'Geoffrey',
+    lastName: 'Butler',
+    link: '#',
+    image: '/images/people-img.jpg',
+    alt: 'Image needs alt text',
+    jobTitle: 'Butler, Former Olympic Runner',
+    email: 'geoffrey.butler@freshprince.tv',
+    phone: '613-520-2600',
+    phoneExt: '3825',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+  {
+    id: 12,
+    type: 'news',
+    title: 'End-of-Year Reflections and Resolutions for Our Web Services Team',
+    link: '#',
+    image: '/images/news-img.jpg',
+    alt: 'Image alt text',
+    excerpt:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam nulla eget est viverra, et tristique velit elementum. Aliquam ultrices vitae felis sit amet pharetra. Vivamus vitae dui mi. Quisque vitae nibh at orci lobortis rhoncus eu at mi. Nulla facilisi. In hac habitasse platea dictumst. Curabitur sed lectus risus.',
+    date: '2022-12-23 10:27:31',
+    tags: {
+      category: [
+        {
+          id: 3,
+          name: 'Category One',
+          slug: 'category-one',
+        },
+        {
+          id: 4,
+          name: 'Category Two',
+          slug: 'category-two',
+        },
+      ],
+    },
+  },
+]
