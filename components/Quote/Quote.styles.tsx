7886e2dfd034b25e2fcb2d3d7493fc837f51d980
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-09-12 08:27:20 -0400

    update: add additional classes for the quote component

--- components/Quote/Quote.styles.tsx
+++ components/Quote/Quote.styles.tsx
@@ -1,4 +1,7 @@
 export const styles = {
-  quote: ``,
-  symbol: `text-7xl`,
+  container: `flex justify-center relative my-10`,
+  quote: `text-center text-2xl`,
+  attribution: `text-right`,
+  symbol: `relative bottom-6 left-0 text-7xl`,
+  symbolend: 'relative bottom-3 left-0 text-7xl rotate-180	',
 }
