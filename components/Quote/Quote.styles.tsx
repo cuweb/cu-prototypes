e8202a2fbbd8e558820050d909f25d3ec5f2d8b8
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-09-13 18:04:46 -0400

    change: update the styles file to remove uneeded classes

--- components/Quote/Quote.styles.tsx
+++ components/Quote/Quote.styles.tsx
@@ -1,11 +1,14 @@
 export const styles = {
   container: `not-prose relative cu-quote`, // done
 
-  // container: `relative flex w-1/2 my-10 cu-quote`, // done
-  // quote: `text-center text-2xl leading-tight`,
-  // attribution: `relative font-semibold`,
-  // symbol: `relative bottom-6 -left-2 text-7xl`,
-  // bar: 'border-l-8 rounded-l-lg mr-3',
-  // black: 'fill-black border-black',
-  // red: 'fill-red-600 border-red-600',
+  symbol: {
+    default: 'border-l-8 rounded-l-lg mr-3 fill-red-600 border-red-600',
+    quote:
+      'bg-quote-icon bg-no-repeat bg-[length:1.5em]	bg-left-top border-none pt-7',
+  },
+
+  cite: {
+    default: '',
+    quote: 'font-semibold',
+  },
 }
