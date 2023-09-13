91c86079b68ae89c975d042ff65cbf53a875b107
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-09-12 21:06:35 -0400

    change: update styling to make better

--- components/Quote/Quote.styles.tsx
+++ components/Quote/Quote.styles.tsx
@@ -1,8 +1,9 @@
 export const styles = {
-  container: `flex w-3/5 justify-center relative my-10`,
-  quote: `text-center text-2xl`,
-  attribution: `relative my-0 float-right`,
+  container: `flex w-1/2 relative my-10`,
+  quote: `text-center text-2xl leading-tight`,
+  attribution: `relative font-semibold`,
   symbol: `relative bottom-6 left-0 text-7xl`,
-  symbolend: 'bottom-0 left-0 text-7xl rotate-180',
-  red: 'fill-red-600',
+  bar: 'border-l-8 rounded-l-lg mr-3',
+  black: 'fill-black border-black',
+  red: 'fill-red-600 border-red-600',
 }
