f08658772d97f1a577b229e78f1a81d069cac016
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-09-14 17:04:59 -0400

    change: update styles for not-prose fix

--- components/Quote/Quote.styles.tsx
+++ components/Quote/Quote.styles.tsx
@@ -1,14 +1,14 @@
 export const styles = {
-  container: `not-prose relative cu-quote`, // done
+  container: `relative cu-quote`, // done
 
   symbol: {
-    default: 'border-l-6 rounded-s fill-red-600 border-red-600',
+    default: 'border-l-4 pl-4 rounded-s fill-red-600 border-red-600',
     quote:
       'bg-quote-icon bg-no-repeat bg-[length:1.5em]	bg-[left_top] border-none pt-8',
   },
 
   align: {
-    default: '',
+    left: 'max-w-3xl mr-auto',
     middle: 'max-w-3xl mx-auto',
     right: 'max-w-3xl ml-auto',
   },
