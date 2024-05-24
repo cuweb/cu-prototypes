21c2367f55912a021a69a6f32bbd525dff0d094f
Troy Chaplin <troy.chaplin@gmail.com>
2024-05-24 14:02:28 -0400

    change: event index

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Link from 'next/link'
+import { useCallback, useState } from 'react'
+import {
+  Main,
+  LinkProvider,
+  Nav,
+  Section,
+  PageHeaders,
+  Search,
+} from '@carletonuniversity/rds'
+import { SearchData } from '../../data/SearchData'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [, setOpen] = useState(false)
+
+  const callback = useCallback(
+    (itemOpen: boolean) => {
+      setOpen(itemOpen)
+    },
+    [setOpen],
+  )
+
+  return (
+    <>
+      <LinkProvider type={Link}>
+        <Nav navType="primary">
+          <Nav.Logo title="Events Calendar" link="/events" />
+          <Nav.Primary>
+            <Nav.Aside LoggedOutUser={true}>
+              <Search sourceData={SearchData} callback={callback} />
+            </Nav.Aside>
+          </Nav.Primary>
+        </Nav>
+      </LinkProvider>
+      <Main>{children}</Main>
+    </>
+  )
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Section, Column, PageHeaders, Card } from '@carletonuniversity/rds'
-import Link from 'next/link'
 
 export default function Page() {
   return (
@@ -14,40 +13,74 @@ export default function Page() {
               <Card.Excerpt text="Features a wide image banner to showcase an important event, followed by 3-4 additional featured events along with a paginated list of filterable events." />
             </Card.Body>
             <Card.Footer>
-              <Link
+              <a
                 className="cu-button cu-button--red cu-button--small"
                 href="/events/homepage-one"
               >
                 View Prototype
-              </Link>
+              </a>
             </Card.Footer>
           </Card>
+          {/* <Card>
+            <Card.Header title="Homepage option two" />
+            <Card.Body>
+              <Card.Excerpt text="This is a work in progress and may not be required should option one be approved." />
+            </Card.Body>
+            <Card.Footer>
+              <a
+                className="cu-button cu-button--red cu-button--small"
+                href="/events/homepage-two"
+              >
+                View Prototype
+              </a>
+            </Card.Footer>
+          </Card> */}
+        </Column>
+      </Section>
+
+      <Section maxWidth="7xl" isGrey>
+        <PageHeaders as="h2" header="Single Event Page Prototypes" size="md" />
+        <Column maxWidth="7xl" cols="3">
           <Card>
             <Card.Header title="cutheme style" />
             <Card.Body>
               <Card.Excerpt text="This prototype reflects a single event view in cutheme." />
             </Card.Body>
             <Card.Footer>
-              <Link
+              <a
                 className="cu-button cu-button--red cu-button--small"
                 href="/events/single-cutheme"
               >
                 View Prototype
-              </Link>
+              </a>
             </Card.Footer>
           </Card>
           <Card>
-            <Card.Header title="Dashboard" />
+            <Card.Header title="Banner with image" />
             <Card.Body>
-              <Card.Excerpt text="This prototype shows the event calendar dashboard view" />
+              <Card.Excerpt text="This example uses the wide image component with an optional background image." />
             </Card.Body>
             <Card.Footer>
-              <Link
+              <a
                 className="cu-button cu-button--red cu-button--small"
-                href="/dashboard"
+                href="/events/single-image"
               >
                 View Prototype
-              </Link>
+              </a>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Header title="Banner without an image" />
+            <Card.Body>
+              <Card.Excerpt text="When no banner image is set the components default light grey background is used." />
+            </Card.Body>
+            <Card.Footer>
+              <a
+                className="cu-button cu-button--red cu-button--small"
+                href="/events/single-light"
+              >
+                View Prototype
+              </a>
             </Card.Footer>
           </Card>
         </Column>
