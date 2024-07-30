5b06617339df1afd9e6dfcb1c29588fe281ab3e7
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-30 18:58:43 -0400

    refactor: homepage components folder

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -6,7 +6,9 @@ import { ButtonGroup, Button } from '@carletonuniversity/rds'
 import PlaceHolder from '@/app/homepage/includes/PlaceHolder'
 import HomeStats from '@/app/homepage/includes/HomeStats'
 import HomeNews from '@/app/homepage/includes/HomeNews'
-import FullBanner from '@/components-homepage/FullBanner'
+
+// New Components for RDS
+import FullBanner from '@/app/homepage/components/FullBanner'
 
 export default function Page() {
   return (
@@ -17,9 +19,12 @@ export default function Page() {
           <Button color="grey" title="Request Information" />
         </ButtonGroup>
       </FullBanner>
+
       <PlaceHolder name="Announcement / Boxed Ad" />
+
       <HomeStats />
       <HomeNews />
+
       <PlaceHolder name="Red Wave" maxWidth="full" />
     </>
   )
