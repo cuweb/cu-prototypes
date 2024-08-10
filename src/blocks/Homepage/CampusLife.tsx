4b5f7ae4a85dd614cb3f5fb6b98263eff10c608e
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-10 10:47:49 -0400

    add: campus life image grid

--- src/blocks/Homepage/CampusLife.tsx
+++ src/blocks/Homepage/CampusLife.tsx
@@ -1,17 +1,64 @@
 import React from 'react'
-import { Section, PageHeader } from '@carletonuniversity/rds'
-import PlaceHolder from '@/components/Placeholder/Placeholder'
+import { Section, PageHeader, ImageGrid } from '@carletonuniversity/rds'
+import Image from 'next/image'
 
 export default function CampusLife() {
   return (
     <Section maxWidth="7xl">
       <PageHeader
-        as="h1"
+        as="h2"
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis."
-        header="Page Header"
-        size="lg"
+        header="Campus Life"
+        size="md"
       />
-      <PlaceHolder name="Campus Life" maxWidth="full" />
+      <ImageGrid cols="4" gridGap="2" maxWidth="5xl">
+        <ImageGrid.Image
+          colSpan="2"
+          content="Lorem ipsum odor amet, consectetuer adipiscing elit. Porta pulvinar consectetur faucibus fusce scelerisque nulla!"
+          link="https://github.com/cuweb/rds"
+          rowSpan="2"
+          title="Overlay Image Title"
+        >
+          <Image
+            width="600"
+            height="400"
+            alt="test"
+            src="https://picsum.photos/id/337/600/400"
+          />
+        </ImageGrid.Image>
+        <ImageGrid.Image>
+          <Image
+            width="600"
+            height="400"
+            alt="Image requirement"
+            src="https://picsum.photos/id/531/600/400"
+          />
+        </ImageGrid.Image>
+        <ImageGrid.Image>
+          <Image
+            width="600"
+            height="400"
+            alt="Image requirement"
+            src="https://picsum.photos/id/626/600/400"
+          />
+        </ImageGrid.Image>
+        <ImageGrid.Image>
+          <Image
+            width="600"
+            height="400"
+            alt="Image requirement"
+            src="https://picsum.photos/id/381/600/400"
+          />
+        </ImageGrid.Image>
+        <ImageGrid.Image>
+          <Image
+            width="600"
+            height="400"
+            alt="Image requirement"
+            src="https://picsum.photos/id/691/600/400"
+          />
+        </ImageGrid.Image>
+      </ImageGrid>
     </Section>
   )
 }
