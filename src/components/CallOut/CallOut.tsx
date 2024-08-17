80208dcd424394b7b0a07e9488480faa111322b8
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-17 01:12:46 -0400

    finish: final touchups

--- src/components/CallOut/CallOut.tsx
+++ src/components/CallOut/CallOut.tsx
@@ -20,7 +20,7 @@ export default function CallOut({
 }: CallOutProps) {
   return (
     <div
-      className={`not-contained cu-component-spacing cu-callout cu-callout--${justify} ${maxWidthClasses[maxWidth]} mx-auto border-8 md:border-[12px] border-solid border-cu-black-50 px-8 pt-5 pb-6 md:px-16 md:pt-8 md:py-10 rounded-lg bg-white`}
+      className={`cu-callout cu-callout--${justify} cu-component-spacing not-contained ${maxWidthClasses[maxWidth]} mx-auto border-8 md:border-[12px] border-solid border-cu-black-50 px-8 pt-5 pb-6 md:px-16 md:pt-8 md:py-10 rounded-lg bg-white`}
     >
       <PageHeader
         as="h2"
